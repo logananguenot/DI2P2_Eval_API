a6c0f5bdb95bad81ed69a43462c5528ba11dc356
Siarrax <logan.anguenot22@gmail.com>
2024-03-06 16:26:11 +0100

    Add EventFunctions and update project file
    A new file, EventFunctions.cs, was created to handle the AddEvent function. The function enables the addition of new events. Concurrently, the Di2P2Eval.Azure.csproj project file was updated to include new package references, in particular Microsoft.Azure.Functions.Worker.Extensions.Timer and Newtonsoft.Json, and the removal of an unused folder.

---
+++ Di2P2Eval.Azure/Functions/EventFunctions.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using Di2P2Eval.Models;
+using Di2P2Eval.Services.Contracts;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+
+namespace Di2P2Eval.Azure.Functions;
+
+public class EventFunctions
+{
+    private readonly IEventService _eventService;
+    private readonly ILogger _logger;
+
+    public EventFunctions(ILoggerFactory loggerFactory, IEventService eventService)
+    {
+        _logger = loggerFactory.CreateLogger<EventFunctions>();
+        _eventService = eventService;
+    }
+
+    [Function("AddEvent")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "Events")] HttpRequestData req)
+    {
+        string requestBody;
+        using (var reader = new StreamReader(req.Body, Encoding.UTF8))
+        {
+            requestBody = await reader.ReadToEndAsync();
+        }
+        try
+        {
+            var response = req.CreateResponse();
+            var evt = JsonSerializer.Deserialize<Event>(requestBody, new JsonSerializerOptions());
+
+            response.StatusCode = HttpStatusCode.Created;
+            await _eventService.AddEvent(evt);
+            return response;
+        }
+        catch (Exception e)
+        {
+            var response = req.CreateResponse();
+            response.WriteString("An error occurred in AddEvent");
+            response.StatusCode = HttpStatusCode.InternalServerError;
+            return response;
+        }
+    }
+}
